aaebf56f948f1451090579d4bba0f831b6ae4bb0
kaushik asokan <kaushikam@gmail.com>
2021-03-12 14:05:50 +0530

    Adding elapsed time

--- cmd/archive/main.go
+++ cmd/archive/main.go
@@ -1,12 +1,20 @@
 package main
 
-import "github.com/mholt/archiver/v3"
+import (
+	"fmt"
+	"time"
+
+	"github.com/mholt/archiver/v3"
+)
 
 func main() {
+	start := time.Now()
 	archiveDetails := GetArchiveDetails()
 	fileList := archiveDetails.FileList()
 	err := archiver.Archive(fileList, archiveDetails.TarGz())
 	if err != nil {
 		panic(err)
 	}
+	elapsed := time.Since(start)
+	fmt.Printf("Archived in %s\n", elapsed)
 }
